f4e27726fd5b08b586a232f99dd0dd3b632b1a29
Igor Malyarov <igor1309@gmail.com>
2021-08-03 13:25:40 +0300

    tweak chatKeyboardWithAutoScrollTo

--- Sources/ChatLibrary/chatKeyboardWithAutoScrollTo.swift
+++ Sources/ChatLibrary/chatKeyboardWithAutoScrollTo.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 public extension View {
-    #warning("how to track text height change? ad use height for tracking, not test itself")
+#warning("how to track text height change? ad use height for tracking, not test itself")
     /// Attach chat keyboard with auto scrolling to view.
     func chatKeyboardWithAutoScrollTo<ID: Hashable>(
         _ id: ID?,
@@ -19,21 +19,22 @@ public extension View {
     ) -> some View {
         
         func scroll(proxy: ScrollViewProxy) {
-            withAnimation {
+            // additional animation here leads to self jiggle when second animation invoked after text change animation
+            // withAnimation {
+            // need a delay to let text change animation (in case of paste or other programmatic change) finish
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
                 if let id = id {
                     proxy.scrollTo(id, anchor: .bottom)
                 }
             }
+            // }
         }
         
         return ScrollViewReader { proxy in
             self
             // TODO: how to track text height change?
                 .onChange(of: text.wrappedValue) { _ in
-                    // need a delay to let text change animation (in case of paste or other programmatic change) finish
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                        scroll(proxy: proxy)
-                    }
+                    scroll(proxy: proxy)
                 }
                 .autoScrollTo(id, anchor: anchor)
                 .chatKeyboard(text: text, maxHeight: maxHeight, action: action)
